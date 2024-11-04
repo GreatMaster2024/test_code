9c07fca8e547b4e7fb744e8a62c4ef414249465e
Sunday Ngwobia <ngwobias@wittenberg.edu>
2024-11-04 13:11:58 -0500

    Initial source code

---
+++ dice_roll.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //result variable
+    int result=1;
+    //welcome display
+    cout<<"Welcome to random number generator!\n";
+   // display dice result
+   cout<<"You rolled :"<<result<<endl;
+   return 0;
+}
